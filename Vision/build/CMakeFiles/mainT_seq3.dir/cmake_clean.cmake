4e411fd77356ee8250c45865f1acf7a818c5be90
student <me@gmail.com>
2018-12-03 00:56:52 +0100

    Creation and configuration of the Vision part

---
+++ Vision/build/CMakeFiles/mainT_seq3.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mainT_seq3.dir/src/test_marker3.cpp.o"
+  "mainT_seq3.pdb"
+  "mainT_seq3"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mainT_seq3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
