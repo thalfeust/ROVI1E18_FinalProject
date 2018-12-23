abce6284f7dbbb5ccb37f6eed7ed24da6a0f9601
Thalfeust <me@gmail.com>
2018-12-23 21:15:36 +0100

    **Robotic :** Add main option to select pathe : _./main Slow_ to use the slow path _./main Medium_ to use the medium path _./main Fast_ to use the fast path

--- Robotic/src/Tracking.cpp
+++ Robotic/src/Tracking.cpp
@@ -1,277 +1,277 @@
 #include "Tracking.h"
 
 void Tracking::getTransformMotions( std::string path) {
-  // Open the file
-  std::ifstream myfile;
-  myfile.open (path, std::ios::in);
+        // Open the file
+        std::ifstream myfile;
+        myfile.open (path, std::ios::in);
 
-  // Initialization of the output
-  std::vector<rw::math::Transform3D<double>> TransformMotionsCurrent;
+        // Initialization of the output
+        std::vector<rw::math::Transform3D<double> > TransformMotionsCurrent;
 
-  if (myfile.is_open()) { /* ok, proceed with output */
+        if (myfile.is_open()) { /* ok, proceed with output */
 
-    std::string line;
+                std::string line;
 
-    // parser
-    while ( getline( myfile, line)) {
+                // parser
+                while ( getline( myfile, line)) {
 
-      std::vector<std::string> tokens;
+                        std::vector<std::string> tokens;
 
-      std::istringstream iss(line);
-      std::string token;
-      while( std::getline(iss, token, '\t')) {
-          tokens.push_back( token);
-      }
+                        std::istringstream iss(line);
+                        std::string token;
+                        while( std::getline(iss, token, '\t')) {
+                                tokens.push_back( token);
+                        }
 
-      // Creaion of P and R from T by the syntax X Y Z Roll Pitch Yaw
-      rw::math::Vector3D<double> P( atof(tokens[0].c_str()), atof(tokens[1].c_str()), atof(tokens[2].c_str()));
-      rw::math::RPY<double> R( atof(tokens[3].c_str()), atof(tokens[4].c_str()), atof(tokens[5].c_str()));
+                        // Creaion of P and R from T by the syntax X Y Z Roll Pitch Yaw
+                        rw::math::Vector3D<double> P( atof(tokens[0].c_str()), atof(tokens[1].c_str()), atof(tokens[2].c_str()));
+                        rw::math::RPY<double> R( atof(tokens[3].c_str()), atof(tokens[4].c_str()), atof(tokens[5].c_str()));
 
-      // Get P and T
-      rw::math::Transform3D<double> T( P, R.toRotation3D());
-      TransformMotionsCurrent.push_back(T);
-    }
+                        // Get P and T
+                        rw::math::Transform3D<double> T( P, R.toRotation3D());
+                        TransformMotionsCurrent.push_back(T);
+                }
 
-    myfile.close();
-  }else {
-    RW_THROW("Motions file not found!");
-  }
+                myfile.close();
+        }else {
+                RW_THROW("Motions file not found!");
+        }
 
-  // Print the results
-  for(unsigned int j = 0; j < TransformMotionsCurrent.size(); ++j) {
-    std::cout << "Ts : " << j << " \n" << TransformMotionsCurrent[j] << std::endl;
-  }
+        // Print the results
+        for(unsigned int j = 0; j < TransformMotionsCurrent.size(); ++j) {
+                std::cout << "Ts : " << j << " \n" << TransformMotionsCurrent[j] << std::endl;
+        }
 
-  // update the object
-  TransformMotions = TransformMotionsCurrent;
+        // update the object
+        TransformMotions = TransformMotionsCurrent;
 }
 
 void Tracking::testError_from_deltaT() {
-  for( float i=0.05; i<=1; i+=0.05) {
-    device->setQ( qInit, state);
-    deltaT = i;
-    errorDUV.clear();
+        for( float i=0.05; i<=1; i+=0.05) {
+                device->setQ( qInit, state);
+                deltaT = i;
+                errorDUV.clear();
 
-    superLoop( true);
-    //std::cout << deltaT << " : [";
-    std::cout << "[";
-    for( int j=0; j<errorDUV.size(); j++) {
-      std::cout << errorDUV[j] << ",";
-    }
-    std::cout << ";\n";
-  }
-  std::cout << "]\n";
+                superLoop( true);
+                //std::cout << deltaT << " : [";
+                std::cout << "[";
+                for( int j=0; j<errorDUV.size(); j++) {
+                        std::cout << errorDUV[j] << ",";
+                }
+                std::cout << ";\n";
+        }
+        std::cout << "]\n";
 }
 
 // juste the main to loop to get each dQ.
 void Tracking::superLoop( bool optionStoreTest) {
 
-    // Get the velocity limit
-    rw::math::Q qVelocityLimit = device->getVelocityLimits();
+        // Get the velocity limit
+        rw::math::Q qVelocityLimit = device->getVelocityLimits();
 
-    // For each steps the new q is store
-    rw::math::Q qStorage[TransformMotions.size()];
-    int accessible[TransformMotions.size()]; // 0 : impossible, 1 : not reachable, 2 : yes
+        // For each steps the new q is store
+        rw::math::Q qStorage[TransformMotions.size()];
+        int accessible[TransformMotions.size()]; // 0 : impossible, 1 : not reachable, 2 : yes
 
-    q = device->getQ(state);
-    for (unsigned int i=0; i<TransformMotions.size(); i++) {
+        q = device->getQ(state);
+        for (unsigned int i=0; i<TransformMotions.size(); i++) {
 
-      dq_from_dUV_computation result = algorithm1( i);
+                dq_from_dUV_computation result = algorithm1( i);
 
-      if(result.dq.size()>0) {
-          q += result.dq[result.dq.size()-1];
-      }
+                if(result.dq.size()>0) {
+                        q += result.dq[result.dq.size()-1];
+                }
 
-    // Update the storages;
-    qStorage[i] = q;
+                // Update the storages;
+                qStorage[i] = q;
 
-    // Update the workcell
-    device->setQ( q, state);
+                // Update the workcell
+                device->setQ( q, state);
 
-    if( optionStoreTest) {
-      errorDUV.push_back( result.maxError);
-    }
+                if( optionStoreTest) {
+                        errorDUV.push_back( result.maxError);
+                }
 
-  }
-  if( !optionStoreTest) {
-    // Print the results
-    std::cout << "\n\n\nQ results : " << std::endl;
-    for (unsigned int i=0; i<TransformMotions.size(); i++) {
-      std::cout << i << " : " << qStorage[i] << "\nCam : " << cam_frame->wTf(state) << std::endl;
-    }
-  }
+        }
+        if( !optionStoreTest) {
+                // Print the results
+                std::cout << "\n\n\nQ results : " << std::endl;
+                for (unsigned int i=0; i<TransformMotions.size(); i++) {
+                        std::cout << i << " : " << qStorage[i] << "\nCam : " << cam_frame->wTf(state) << std::endl;
+                }
+        }
 }
 
 // based on the ex4_2 correction
 dq_from_dUV_computation Tracking::algorithm1( int index) {
 
-  // Initialization of the return variable
-  dq_from_dUV_computation results;
-  results.iterations = 0;
+        // Initialization of the return variable
+        dq_from_dUV_computation results;
+        results.iterations = 0;
 
-  rw::math::Q currentQ = q;
-  rw::math::Q currentdQ;
+        rw::math::Q currentQ = q;
+        rw::math::Q currentdQ;
 
-  double maxError = 0;
+        double maxError = 0;
 
-  rw::math::Q qVelocityLimit = device->getVelocityLimits();
+        rw::math::Q qVelocityLimit = device->getVelocityLimits();
 
-    // Comute du and dv
-    rw::math::Vector2D<double> currentDUV = get_du_dv( index);
+        // Comute du and dv
+        rw::math::Vector2D<double> currentDUV = get_du_dv( index);
 
-  const double epsilon = 1; // precision of 1 pixel
+        const double epsilon = 1; // precision of 1 pixel
 
-  // Loop until the precision is lower 1 pixel
-  while( currentDUV.norm2() > epsilon) {
+        // Loop until the precision is lower 1 pixel
+        while( currentDUV.norm2() > epsilon) {
 
-    // Get Zimage from the equation following the (eq.4.34)
-    rw::math::Jacobian Zimage = get_Zimage( );
+                // Get Zimage from the equation following the (eq.4.34)
+                rw::math::Jacobian Zimage = get_Zimage( );
 
-    // based on the way to solve the equation (eq.4.34)
-    rw::math::Q dq( rw::math::LinearAlgebra::pseudoInverse(Zimage.e())*currentDUV.e());
+                // based on the way to solve the equation (eq.4.34)
+                rw::math::Q dq( rw::math::LinearAlgebra::pseudoInverse(Zimage.e())*currentDUV.e());
 
-    // Update the result of the function
-    if(results.iterations > 1) {
-        currentdQ += dq;
-    }else {
-        currentdQ = dq;
-    }
+                // Update the result of the function
+                if(results.iterations > 1) {
+                        currentdQ += dq;
+                }else {
+                        currentdQ = dq;
+                }
 
-    bool reachable = true;
-    for (unsigned w=0; w<q.size(); w++) {
-      if( currentdQ[w] / deltaT > qVelocityLimit[w]) {
-        reachable = false;
-        break;
-      }
-    }
+                bool reachable = true;
+                for (unsigned w=0; w<q.size(); w++) {
+                        if( currentdQ[w] / deltaT > qVelocityLimit[w]) {
+                                reachable = false;
+                                break;
+                        }
+                }
 
-    // The robot can't reach the position
-    if( !reachable){
-      break;
-    }
+                // The robot can't reach the position
+                if( !reachable) {
+                        break;
+                }
 
-    if(results.iterations > 1) {
-        results.dq.push_back( dq+results.dq[ results.iterations+1]);
-    }else {
-        results.dq.push_back( dq);
-    }
-    results.iterations++;
+                if(results.iterations > 1) {
+                        results.dq.push_back( dq+results.dq[ results.iterations+1]);
+                }else {
+                        results.dq.push_back( dq);
+                }
+                results.iterations++;
 
-    // Update the state of the joints
-    currentQ = device->getQ( state) + dq;
-    device->setQ( currentQ, state);
+                // Update the state of the joints
+                currentQ = device->getQ( state) + dq;
+                device->setQ( currentQ, state);
 
-    //std::cout << "dQ from Algo1 : [" << results.iterations << "]\n" << dq << std::endl;
+                //std::cout << "dQ from Algo1 : [" << results.iterations << "]\n" << dq << std::endl;
 
-    // Compute the new du and dv
-    currentDUV = get_du_dv( index);
+                // Compute the new du and dv
+                currentDUV = get_du_dv( index);
 
-    //update the ouput
-    maxError = currentDUV.norm2();
+                //update the ouput
+                maxError = currentDUV.norm2();
 
-    //std::cout << "currentDUV from Algo1 : \n" << currentDUV << std::endl;
-  }
-  results.maxError = maxError;
+                //std::cout << "currentDUV from Algo1 : \n" << currentDUV << std::endl;
+        }
+        results.maxError = maxError;
 
-  return results;
+        return results;
 }
 
 rw::math::Jacobian Tracking::get_Zimage() {
 
-  // get the Jacobian
-  rw::math::Jacobian J = device->baseJframe( cam_frame, state);
+        // get the Jacobian
+        rw::math::Jacobian J = device->baseJframe( cam_frame, state);
 
-  // get the matrix Sq from baseRcam
-  rw::math::Jacobian Sq = get_Sq();
+        // get the matrix Sq from baseRcam
+        rw::math::Jacobian Sq = get_Sq();
 
-  // get Jimage from UV before displacement
-  rw::math::Jacobian Jimage = get_Jimage();
-  rw::math::Jacobian Jimage_transpose(Jimage.e().transpose());
+        // get Jimage from UV before displacement
+        rw::math::Jacobian Jimage = get_Jimage();
+        rw::math::Jacobian Jimage_transpose(Jimage.e().transpose());
 
-  // return Zimage
-  return Jimage_transpose * Sq * J;
+        // return Zimage
+        return Jimage_transpose * Sq * J;
 }
 
 rw::math::Jacobian Tracking::get_Sq() {
 
-  rw::math::Jacobian Sq(6);
-  rw::math::Rotation3D<> R = device->baseTframe(cam_frame, state).R();
+        rw::math::Jacobian Sq(6);
+        rw::math::Rotation3D<> R = device->baseTframe(cam_frame, state).R();
 
-  Sq(0,0) = R(0,0);
-  Sq(1,0) = R(0,1);
-  Sq(2,0) = R(0,2);
-  Sq(0,1) = R(1,0);
-  Sq(1,1) = R(1,1);
-  Sq(2,1) = R(1,2);
-  Sq(0,2) = R(2,0);
-  Sq(1,2) = R(2,1);
-  Sq(2,2) = R(2,2);
+        Sq(0,0) = R(0,0);
+        Sq(1,0) = R(0,1);
+        Sq(2,0) = R(0,2);
+        Sq(0,1) = R(1,0);
+        Sq(1,1) = R(1,1);
+        Sq(2,1) = R(1,2);
+        Sq(0,2) = R(2,0);
+        Sq(1,2) = R(2,1);
+        Sq(2,2) = R(2,2);
 
-  Sq(3,0) = 0;
-  Sq(4,0) = 0;
-  Sq(5,0) = 0;
-  Sq(3,1) = 0;
-  Sq(4,1) = 0;
-  Sq(5,1) = 0;
-  Sq(3,2) = 0;
-  Sq(4,2) = 0;
-  Sq(5,2) = 0;
+        Sq(3,0) = 0;
+        Sq(4,0) = 0;
+        Sq(5,0) = 0;
+        Sq(3,1) = 0;
+        Sq(4,1) = 0;
+        Sq(5,1) = 0;
+        Sq(3,2) = 0;
+        Sq(4,2) = 0;
+        Sq(5,2) = 0;
 
-  Sq(0,3) = 0;
-  Sq(1,3) = 0;
-  Sq(2,3) = 0;
-  Sq(0,4) = 0;
-  Sq(1,4) = 0;
-  Sq(2,4) = 0;
-  Sq(0,5) = 0;
-  Sq(1,5) = 0;
-  Sq(2,5) = 0;
+        Sq(0,3) = 0;
+        Sq(1,3) = 0;
+        Sq(2,3) = 0;
+        Sq(0,4) = 0;
+        Sq(1,4) = 0;
+        Sq(2,4) = 0;
+        Sq(0,5) = 0;
+        Sq(1,5) = 0;
+        Sq(2,5) = 0;
 
-  Sq(3,3) = R(0,0);
-  Sq(4,3) = R(0,1);
-  Sq(5,3) = R(0,2);
-  Sq(3,4) = R(1,0);
-  Sq(4,4) = R(1,1);
-  Sq(5,4) = R(1,2);
-  Sq(3,5) = R(2,0);
-  Sq(4,5) = R(2,1);
-  Sq(5,5) = R(2,2);
+        Sq(3,3) = R(0,0);
+        Sq(4,3) = R(0,1);
+        Sq(5,3) = R(0,2);
+        Sq(3,4) = R(1,0);
+        Sq(4,4) = R(1,1);
+        Sq(5,4) = R(1,2);
+        Sq(3,5) = R(2,0);
+        Sq(4,5) = R(2,1);
+        Sq(5,5) = R(2,2);
 
-  return Sq;
+        return Sq;
 }
 
 rw::math::Jacobian Tracking::get_Jimage() {
 
-  rw::math::Jacobian Jimage(2);
-  Jimage(0,0) = -f/z;
-  Jimage(1,0) = 0;
-  Jimage(2,0) = UVref[0]/z;
-  Jimage(3,0) = UVref[0]*UVref[1]/f;
-  Jimage(4,0) = -1 * ((f*f+UVref[0]*UVref[0])/f);
-  Jimage(5,0) = UVref[1];
-  Jimage(0,1) = 0;
-  Jimage(1,1) = -f/z;
-  Jimage(2,1) = UVref[1]/z;
-  Jimage(3,1) = ((f*f+UVref[1]*UVref[1])/f);
-  Jimage(4,1) = -(UVref[0]*UVref[1])/f;
-  Jimage(5,1) = -UVref[0];
+        rw::math::Jacobian Jimage(2);
+        Jimage(0,0) = -f/z;
+        Jimage(1,0) = 0;
+        Jimage(2,0) = UVref[0]/z;
+        Jimage(3,0) = UVref[0]*UVref[1]/f;
+        Jimage(4,0) = -1 * ((f*f+UVref[0]*UVref[0])/f);
+        Jimage(5,0) = UVref[1];
+        Jimage(0,1) = 0;
+        Jimage(1,1) = -f/z;
+        Jimage(2,1) = UVref[1]/z;
+        Jimage(3,1) = ((f*f+UVref[1]*UVref[1])/f);
+        Jimage(4,1) = -(UVref[0]*UVref[1])/f;
+        Jimage(5,1) = -UVref[0];
 
-  return Jimage;
+        return Jimage;
 }
 
 // This function is used to get the value of a displacement (du,dv) from the coordinate (x,y,z) of a point on the picture in the coordinate frame of the camera.
 rw::math::Vector2D<double> Tracking::get_du_dv(int index) {
 
-  rw::math::Transform3D<double> wTmarker = TransformMotions[index];
+        rw::math::Transform3D<double> wTmarker = TransformMotions[index];
 
-  marker_frame->setTransform( wTmarker, state);
+        marker_frame->setTransform( wTmarker, state);
 
-  // Get new Pos of the marker inside Fcam
-  rw::math::Transform3D<double> markerTcam = cam_frame->fTf( marker_frame, state);
+        // Get new Pos of the marker inside Fcam
+        rw::math::Transform3D<double> markerTcam = cam_frame->fTf( marker_frame, state);
 
-  rw::math::Vector3D<double> P = markerTcam.P();
-  rw::math::Vector2D<double> UV( f*P(0)/P(2), f*P(1)/P(2));
-  rw::math::Vector2D<double> dUV( UVref(0)-UV(0), UVref(1)-UV(1));
+        rw::math::Vector3D<double> P = markerTcam.P();
+        rw::math::Vector2D<double> UV( f*P(0)/P(2), f*P(1)/P(2));
+        rw::math::Vector2D<double> dUV( UVref(0)-UV(0), UVref(1)-UV(1));
 
-  return dUV;
+        return dUV;
 }
--- Robotic/src/main.cpp
+++ Robotic/src/main.cpp
@@ -6,72 +6,90 @@
 
 #include "Tracking.h"
 
-int main() {
+using namespace std;
 
-  Tracking tracker;
+int main( int argc, char *argv[]) {
 
-  // Paths to workcell, name of robot, path for the motion
-  const std::string workcell_path = "../../Workcells/PA10WorkCell/ScenePA10RoVi1.wc.xml";
-  const std::string device_name = "PA10";
-  //const std::string motions_path = "../../Plugin/SamplePluginPA10/motions/MarkerMotionSlow.txt";
-  //const std::string motions_path = "../../Plugin/SamplePluginPA10/motions/MarkerMotionMedium.txt";
-  const std::string motions_path = "../../Plugin/SamplePluginPA10/motions/MarkerMotionFast.txt";
+        std::string motions_path = "";
+        if( argc>1) {
+                if( strcmp( argv[1], "Slow")==0) {
+                        motions_path = "../../Plugin/SamplePluginPA10/motions/MarkerMotionSlow.txt";
+                }else if( strcmp( argv[1], "Medium")==0) {
+                        motions_path = "../../Plugin/SamplePluginPA10/motions/MarkerMotionMedium.txt";
+                }else if( strcmp( argv[1], "Fast")==0) {
+                        motions_path = "../../Plugin/SamplePluginPA10/motions/MarkerMotionFast.txt";
+                }else {
+                        motions_path = "../../Plugin/SamplePluginPA10/motions/MarkerMotionSlow.txt";
+                }
+        }else {
+                motions_path = "../../Plugin/SamplePluginPA10/motions/MarkerMotionSlow.txt";
+        }
 
-  // Load workcell
-  rw::models::WorkCell::Ptr wc = rw::loaders::WorkCellLoader::Factory::load(workcell_path);
+        std::cout << "File path : " << motions_path << "\nPress a key + enter to continu ..." << std::endl;
+        char tamp;
+        std::cin >> tamp;
 
-  // Get device and check if it has been loaded correctly
-  tracker.device = wc->findDevice(device_name);
-  if(tracker.device == nullptr) {
-      RW_THROW("Device " << device_name << " was not found!");
-  }
+        Tracking tracker;
 
-  // Find Camera
-  tracker.cam_frame = wc->findFrame("Camera");
-  if(tracker.cam_frame == nullptr) {
-      RW_THROW("Camera frame not found!");
-  }
+        // Paths to workcell, name of robot, path for the motion
+        const std::string workcell_path = "../../Workcells/PA10WorkCell/ScenePA10RoVi1.wc.xml";
+        const std::string device_name = "PA10";
 
-  // Find Marker as movable to set the news positions
-  tracker.marker_frame = dynamic_cast<rw::kinematics::MovableFrame*>(wc->findFrame("Marker"));
-  if(tracker.marker_frame == nullptr) {
-      RW_THROW("Marker frame not found!");
-  }
+        // Load workcell
+        rw::models::WorkCell::Ptr wc = rw::loaders::WorkCellLoader::Factory::load(workcell_path);
 
-  // Get an initial configuration, q
-  tracker.state = wc->getDefaultState();
-  rw::math::Q qq(7,0,-0.65,0,1.80,0,0.42,0);
-  tracker.q = qq;
-  tracker.qInit = qq;
-  tracker.device->setQ( tracker.q, tracker.state);
-  tracker.deltaT = 1;
-  tracker.f = 823;
-  tracker.z = 0.5;
+        // Get device and check if it has been loaded correctly
+        tracker.device = wc->findDevice(device_name);
+        if(tracker.device == nullptr) {
+                RW_THROW("Device " << device_name << " was not found!");
+        }
 
-  tracker.getTransformMotions( motions_path);
+        // Find Camera
+        tracker.cam_frame = wc->findFrame("Camera");
+        if(tracker.cam_frame == nullptr) {
+                RW_THROW("Camera frame not found!");
+        }
 
-  // Get the motions
-  /*std::vector<rw::math::Transform3D<double>> TransformMotions = getTransformMotions( motions_path);*/
+        // Find Marker as movable to set the news positions
+        tracker.marker_frame = dynamic_cast<rw::kinematics::MovableFrame*>(wc->findFrame("Marker"));
+        if(tracker.marker_frame == nullptr) {
+                RW_THROW("Marker frame not found!");
+        }
 
-  std::cout << "\n\n\n";
+        // Get an initial configuration, q
+        tracker.state = wc->getDefaultState();
+        rw::math::Q qq(7,0,-0.65,0,1.80,0,0.42,0);
+        tracker.q = qq;
+        tracker.qInit = qq;
+        tracker.device->setQ( tracker.q, tracker.state);
+        tracker.deltaT = 1;
+        tracker.f = 823;
+        tracker.z = 0.5;
 
-  // Get Pos of the marker inside Fcam to compute UVref
-  rw::math::Transform3D<double> markerTcam = tracker.cam_frame->fTf( tracker.marker_frame, tracker.state);
-  rw::math::Vector3D<double> P = markerTcam.P();
-  rw::math::Vector2D<double> uvref( tracker.f*P(0)/P(2), tracker.f*P(1)/P(2));
-  tracker.UVref = uvref;
-  tracker.UVref1 = rw::math::Vector2D<double>( tracker.f*(P(0)+0.15)/P(2), tracker.f*P(1)/P(2));
-  tracker.UVref1 = rw::math::Vector2D<double>( tracker.f*(P(0)+0.15)/P(2), tracker.f*(P(1)+0.15)/P(2));
-  tracker.UVref2 = rw::math::Vector2D<double>( tracker.f*(P(0)-0.15)/P(2), tracker.f*(P(1)+0.15)/P(2));
-  tracker.UVref3 = rw::math::Vector2D<double>( tracker.f*(P(0)+0.15)/P(2), tracker.f*(P(1)-0.15)/P(2));
+        tracker.getTransformMotions( motions_path);
+
+        // Get the motions
+        /*std::vector<rw::math::Transform3D<double>> TransformMotions = getTransformMotions( motions_path);*/
+
+        std::cout << "\n\n\n";
+
+        // Get Pos of the marker inside Fcam to compute UVref
+        rw::math::Transform3D<double> markerTcam = tracker.cam_frame->fTf( tracker.marker_frame, tracker.state);
+        rw::math::Vector3D<double> P = markerTcam.P();
+        rw::math::Vector2D<double> uvref( tracker.f*P(0)/P(2), tracker.f*P(1)/P(2));
+        tracker.UVref = uvref;
+        tracker.UVref1 = rw::math::Vector2D<double>( tracker.f*(P(0)+0.15)/P(2), tracker.f*P(1)/P(2));
+        tracker.UVref1 = rw::math::Vector2D<double>( tracker.f*(P(0)+0.15)/P(2), tracker.f*(P(1)+0.15)/P(2));
+        tracker.UVref2 = rw::math::Vector2D<double>( tracker.f*(P(0)-0.15)/P(2), tracker.f*(P(1)+0.15)/P(2));
+        tracker.UVref3 = rw::math::Vector2D<double>( tracker.f*(P(0)+0.15)/P(2), tracker.f*(P(1)-0.15)/P(2));
 
 
-  std::cout << "UVref : " << tracker.UVref << std::endl;
+        std::cout << "UVref : " << tracker.UVref << std::endl;
 
-  //tracker.superLoop(false);
-  tracker.testError_from_deltaT();
+        //tracker.superLoop(false);
+        tracker.testError_from_deltaT();
 
-  std::cout << "END" << std::endl;
-  return 0;
+        std::cout << "END" << std::endl;
+        return 0;
 
 }
